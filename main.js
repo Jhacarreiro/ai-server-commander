637d78c51c7fd602ffd8ce8ad59e03199bd912f3
Zauberio <95449991+Zauberio@users.noreply.github.com>
2026-08-07 10:46:42 +0200

    chore: move startServer() catch to its own PR (#26)

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
 const startServer = require('./serverModules/pluginServer.js');
 
-startServer().catch((error) => {
-    console.error('Failed to start server:', error && error.message ? error.message : error);
-    if (error && error.stack) console.error(error.stack);
-    process.exit(1);
-});
+startServer();
